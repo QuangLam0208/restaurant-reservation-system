3d0c13339811f793be069aa186eb09d93edf9416
Luong Quang Lam <laulua1223@gmail.com>
2026-03-30 00:56:37 +0700

    manage mapping to db n-n reservation and table

--- backend-api/reservation-api/src/main/java/com/hcmute/reservation/service/ReservationMappingService.java
+++ backend-api/reservation-api/src/main/java/com/hcmute/reservation/service/ReservationMappingService.java
@@ -1,4 +1,41 @@
 package com.hcmute.reservation.service;
 
+import com.hcmute.reservation.model.entity.Reservation;
+import com.hcmute.reservation.model.entity.ReservationTableMapping;
+import com.hcmute.reservation.model.entity.TableInfo;
+import com.hcmute.reservation.repository.ReservationTableMappingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReservationMappingService {
+
+    private final ReservationTableMappingRepository mappingRepository;
+
+    @Transactional(propagation = Propagation.MANDATORY) // Bắt buộc chạy trong Transaction của hàm gọi nó
+    public void updateTableMappings(Reservation reservation, List<TableInfo> newTables) {
+        // Xóa mapping cũ
+        if (reservation.getTableMappings() != null && !reservation.getTableMappings().isEmpty()) {
+            mappingRepository.deleteAll(reservation.getTableMappings());
+            reservation.getTableMappings().clear();
+        } else {
+            reservation.setTableMappings(new ArrayList<>());
+        }
+
+        // Tạo mapping mới
+        for (TableInfo t : newTables) {
+            ReservationTableMapping mapping = ReservationTableMapping.builder()
+                    .reservation(reservation)
+                    .tableInfo(t)
+                    .build();
+            mappingRepository.save(mapping);
+            reservation.getTableMappings().add(mapping);
+        }
+    }
 }
