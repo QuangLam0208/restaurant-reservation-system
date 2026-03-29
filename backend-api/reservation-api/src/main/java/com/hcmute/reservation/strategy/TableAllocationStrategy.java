6078fc1541144b1937750e4f870511dc7f931227
Luong Quang Lam <laulua1223@gmail.com>
2026-03-30 00:47:22 +0700

    dip for allocate table

--- backend-api/reservation-api/src/main/java/com/hcmute/reservation/strategy/TableAllocationStrategy.java
+++ backend-api/reservation-api/src/main/java/com/hcmute/reservation/strategy/TableAllocationStrategy.java
@@ -1,4 +1,12 @@
 package com.hcmute.reservation.strategy;
 
-public interface TableAllocationStrategy {
+import com.hcmute.reservation.model.entity.TableInfo;
+import org.springframework.core.Ordered;
+import java.util.List;
+
+public interface TableAllocationStrategy extends Ordered {
+
+    // Trả về danh sách bàn được chọn. Nếu không tìm được, trả về list rỗng.
+    List<TableInfo> allocate(int guestCount, List<TableInfo> freeTables);
+
 }
