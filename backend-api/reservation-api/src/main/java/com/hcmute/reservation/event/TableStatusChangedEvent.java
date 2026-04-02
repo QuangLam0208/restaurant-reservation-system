dd16f9c1989a0f9c8960027979247f96cd053e24
Luong Quang Lam <laulua1223@gmail.com>
2026-04-02 09:34:50 +0700

    event pack data to broadcast

--- backend-api/reservation-api/src/main/java/com/hcmute/reservation/event/TableStatusChangedEvent.java
+++ backend-api/reservation-api/src/main/java/com/hcmute/reservation/event/TableStatusChangedEvent.java
@@ -1,4 +1,15 @@
 package com.hcmute.reservation.event;
 
-public class TableStatusChangedEvent {
+import com.hcmute.reservation.model.dto.table.TableUpdate;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class TableStatusChangedEvent extends ApplicationEvent {
+    private final TableUpdate payload;
+
+    public TableStatusChangedEvent(Object source, Long tableId, String status) {
+        super(source);
+        this.payload = new TableUpdate(tableId, status);
+    }
 }
