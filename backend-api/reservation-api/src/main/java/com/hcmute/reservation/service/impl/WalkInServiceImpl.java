0a0efd074e73da527186c08c4c9e91a82b4c0e6a
Luong Quang Lam <laulua1223@gmail.com>
2026-04-02 14:24:59 +0700

    .

--- backend-api/reservation-api/src/main/java/com/hcmute/reservation/service/impl/WalkInServiceImpl.java
+++ backend-api/reservation-api/src/main/java/com/hcmute/reservation/service/impl/WalkInServiceImpl.java
@@ -23,7 +23,6 @@ import com.hcmute.reservation.service.WalkInService;
 import com.hcmute.reservation.strategy.TableCombinationAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
